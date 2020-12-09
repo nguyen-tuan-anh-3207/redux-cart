7ed47c7852f01450c49fcf1cfd35a40b94d8aa00
Tuan Anh <tuananhnguyen3207@gmail.com>
2020-12-09 16:20:17 +0700

    first redux cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Cart from './components/Cart';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Message from './components/Message';
+import Products from './components/Products';
 
-function App() {
+
+
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <main id="mainContainer">
+            <div className="container">
+                <Products/>
+                <Message/>
+                <Cart/>
+            </div>
+        </main>
+      <Footer/>
     </div>
   );
-}
+}}
 
 export default App;
